5d0226533b9ca51b83b793cd40c31d1b3ac7d2aa
Tom Porter <tomvporter@hotmail.com>
2023-12-01 22:17:32 +0000

    GUI update

--- FileOrganiser.py
+++ FileOrganiser.py
@@ -1,22 +1,41 @@
 import tkinter as tk
 from tkinter import PhotoImage
+import subprocess
 
 background = (r"Prereq\Background.png")
 
+# Tools
+def DocOrg():
+    file_path = 'Tools/Documents.py'
+    subprocess.run(["python", file_path])
+def DowOrg():
+    file_path = 'Tools/Downloads.py'
+    subprocess.run(["python", file_path])
+def PicOrg():
+    file_path = 'Tools/Pictures.py'
+    subprocess.run(["python", file_path])
+
 # Main Tkinter Window
 window = tk.Tk()
-window.title("Forganizer")
-window.geometry("400x200")
+window.title("Forganizer by Tom Porter")
+window.geometry("410x100")
 window.resizable(False, False)
-background_image = PhotoImage(file=background) 
+background_image = PhotoImage(file=background)
 background_label = tk.Label(window, image=background_image)
 background_label.place(relwidth=1, relheight=1)
+
 # Labels
 label_title = tk.Label(window, text="Forganiser")
-label_title.pack(pady=10)
+label_title.grid(row=0, column=0, columnspan=2, pady=10)
 
-label_author = tk.Label(window, text="by Tom Porter")
-label_author.pack(pady=4)
+
+# Buttons
+button1 = tk.Button(window, text="Document Organiser", command=DocOrg)
+button1.grid(row=2, column=1, pady=10, padx=10, sticky="w")
+button2 = tk.Button(window, text="Downloads Organiser", command=DowOrg)
+button2.grid(row=2, column=2, pady=10, padx=10)
+button3 = tk.Button(window, text="Pictures Organiser", command=PicOrg)
+button3.grid(row=2, column=3, pady=10, padx=10)
 
 # Loop Window
 window.mainloop()
