647ecf4c56f91debe3b2478576a703a207d1cd24
Tom Porter <tomvporter@hotmail.com>
2023-12-01 21:57:45 +0000

    Added Downloads Organiser

---
+++ Tools/Downloads.py
@@ -0,0 +1,34 @@
+import os
+import shutil
+
+def organize_downloads_folder():
+    # Get the path to the Downloads folder
+    downloads_folder = os.path.expanduser("~/Downloads")
+
+    # Create a dictionary to store file extensions and their corresponding subdirectories
+    extensions = {}
+
+    # Iterate through files in the Downloads folder
+    for filename in os.listdir(downloads_folder):
+        file_path = os.path.join(downloads_folder, filename)
+
+        # Check if the path is a file (not a directory)
+        if os.path.isfile(file_path):
+            # Get the file extension
+            _, file_extension = os.path.splitext(filename)
+            file_extension = file_extension[1:].lower()  # Remove the dot and convert to lowercase
+
+            # Create a subdirectory if it doesn't exist
+            if file_extension not in extensions:
+                extensions[file_extension] = os.path.join(downloads_folder, file_extension)
+                os.makedirs(extensions[file_extension])
+
+            # Move the file to the corresponding subdirectory
+            destination = os.path.join(extensions[file_extension], filename)
+            shutil.move(file_path, destination)
+            print(f"Moved {filename} to {file_extension} folder.")
+
+    print("Organizing completed.")
+
+if __name__ == "__main__":
+    organize_downloads_folder()
